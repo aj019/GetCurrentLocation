17e8b3053433c62468d8c0162b8f76a35f1c8b27
Anuj Gupta <junagupta19@gmail.com>
2017-02-20 16:14:49 +0530

    Added Fused Api

--- app/src/main/java/com/mate/android/currentlocation/SplashActivity.java
+++ app/src/main/java/com/mate/android/currentlocation/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.mate.android.currentlocation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(SplashActivity.this,MainActivity.class));
+        finish();
     }
 }
